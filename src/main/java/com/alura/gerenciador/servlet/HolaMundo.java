c2ae8ecd7a2504ea85fd4518018f99e316ea0292
Luis Cuellar <luisenri2901@gmail.com>
2023-06-10 23:00:30 -0500

    Se llevó a cabo el primer servlet, para ello se creó una clase que extendiera de HttpServlet, dandole la anotación WebServlet que permite darle el nombre de la url. Aqui también se usó el método service, el cual recibe la request y la response en la conexion, aquí se hizo todo un html basico que mostrara un hola mundo servlet

---
+++ src/main/java/com/alura/gerenciador/servlet/HolaMundo.java
@@ -0,0 +1,35 @@
+package com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//Esta clase va a ser un servlet, extiende de un server del tipo http
+
+@WebServlet(urlPatterns = "/hola") //Anotación para agregar el nombre que vamos  usar para llamar al servlet
+public class HolaMundo extends HttpServlet {
+	
+	//Importamos el método service, el cual tiene la request y response como parametros
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		//Lo que se va enviar
+		PrintWriter out = resp.getWriter();
+		//Imprime en lo que enviamos al navegador
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hola mundo, felicitaciones por crear tu primer Servlet!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("Se ejecutó el servlet");
+		
+	}
+
+}
